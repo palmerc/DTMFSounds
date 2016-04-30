26333aca4b5cbf07c6f6ee609eb773b244ea098b
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-04-30 22:23:59 +0200

    A working DTMF tone generator

---
+++ DTMFSounds.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import AVFoundation
+import XCPlayground
+
+
+
+var _sampleRate: Float = 8000.0
+
+var _engine = AVAudioEngine()
+var _player:AVAudioPlayerNode = AVAudioPlayerNode()
+var _mixer = _engine.mainMixerNode
+
+let phoneNumber = "1234567890*#ABCD"
+if let tones = DTMF.tonesForString(phoneNumber) {
+    let audioFormat = AVAudioFormat(commonFormat: .PCMFormatFloat32, sampleRate: Double(_sampleRate), channels: 2, interleaved: false)
+
+    // fill up the buffer with some samples
+    var allSamples = [Float]()
+    for tone in tones {
+        let samples = DTMF.generateDTMF(frequency1: tone.0, frequency2: tone.1, markSpace: DTMF.motorola, sampleRate: _sampleRate)
+        allSamples.appendContentsOf(samples)
+    }
+
+    let frameCount = AVAudioFrameCount(allSamples.count)
+    var buffer = AVAudioPCMBuffer(PCMFormat: audioFormat, frameCapacity: frameCount)
+
+    buffer.frameLength = frameCount
+    var channelMemory = buffer.floatChannelData
+    for channelIndex in 0 ..< Int(audioFormat.channelCount) {
+        var frameMemory = channelMemory[channelIndex]
+        memcpy(frameMemory, allSamples, Int(frameCount) * sizeof(Float))
+    }
+
+    _engine.attachNode(_player)
+    _engine.connect(_player, to:_mixer, format:audioFormat)
+
+    do {
+        try _engine.start()
+    } catch let error as NSError {
+        print("Engine start failed - \(error)")
+    }
+
+    _player.scheduleBuffer(buffer, atTime:nil, options:.Loops,completionHandler:nil)
+    _player.play()
+}
+
+// keep playground running
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
---
+++ DTMFSounds.playground/Sources/DTMF.swift
@@ -0,0 +1,145 @@
+import Foundation
+
+
+public typealias DTMFType = (Float, Float)
+public typealias MarkSpaceType = (Float, Float)
+public class DTMF
+{
+    public static let tone1     = DTMFType(1209.0, 697.0)
+    public static let tone2     = DTMFType(1336.0, 697.0)
+    public static let tone3     = DTMFType(1477.0, 697.0)
+    public static let tone4     = DTMFType(1209.0, 770.0)
+    public static let tone5     = DTMFType(1336.0, 770.0)
+    public static let tone6     = DTMFType(1477.0, 770.0)
+    public static let tone7     = DTMFType(1209.0, 852.0)
+    public static let tone8     = DTMFType(1336.0, 852.0)
+    public static let tone9     = DTMFType(1477.0, 852.0)
+    public static let tone0     = DTMFType(1336.0, 941.0)
+    public static let toneStar  = DTMFType(1209.0, 941.0)
+    public static let tonePound = DTMFType(1477.0, 941.0)
+    public static let toneA     = DTMFType(1633.0, 697.0)
+    public static let toneB     = DTMFType(1633.0, 770.0)
+    public static let toneC     = DTMFType(1633.0, 852.0)
+    public static let toneD     = DTMFType(1633.0, 941.0)
+
+    public static let standard  = MarkSpaceType(40.0, 40.0)
+    public static let motorola  = MarkSpaceType(250.0, 250.0)
+    public static let whelen    = MarkSpaceType(40.0, 20.0)
+    public static let fast      = MarkSpaceType(20.0, 20.0)
+
+
+
+    public static func generateDTMF(frequency1 frequency1: Float, frequency2: Float, markSpace: MarkSpaceType, sampleRate: Float) -> [Float]
+    {
+        let toneLengthInSamples = 10e-4 * markSpace.0 * sampleRate
+        let silenceLengthInSamples = 10e-4 * markSpace.1 * sampleRate
+
+        var sound = [Float](count: Int(toneLengthInSamples + silenceLengthInSamples), repeatedValue: 0)
+        let twoPI = 2.0 * Float(M_PI)
+
+        for i in 0 ..< Int(toneLengthInSamples) {
+            // Add first tone at half volume
+            let sample1 = 0.5 * sin(Float(i) * twoPI / (sampleRate / frequency1));
+
+            // Add second tone at half volume
+            let sample2 = 0.5 * sin(Float(i) * twoPI / (sampleRate / frequency2));
+
+            sound[i] = sample1 + sample2
+        }
+
+        return sound
+    }
+}
+
+extension DTMF
+{
+    enum characterForTone: Character {
+        case tone1     = "1"
+        case tone2     = "2"
+        case tone3     = "3"
+        case tone4     = "4"
+        case tone5     = "5"
+        case tone6     = "6"
+        case tone7     = "7"
+        case tone8     = "8"
+        case tone9     = "9"
+        case tone0     = "0"
+        case toneA     = "A"
+        case toneB     = "B"
+        case toneC     = "C"
+        case toneD     = "D"
+        case toneStar  = "*"
+        case tonePound = "#"
+    }
+
+    public static func toneForCharacter(character: Character) -> DTMFType?
+    {
+        var tone: DTMFType?
+        switch (character) {
+        case characterForTone.tone1.rawValue:
+            tone = DTMF.tone1
+            break
+        case characterForTone.tone2.rawValue:
+            tone = DTMF.tone2
+            break
+        case characterForTone.tone3.rawValue:
+            tone = DTMF.tone3
+            break
+        case characterForTone.tone4.rawValue:
+            tone = DTMF.tone4
+            break
+        case characterForTone.tone5.rawValue:
+            tone = DTMF.tone5
+            break
+        case characterForTone.tone6.rawValue:
+            tone = DTMF.tone6
+            break
+        case characterForTone.tone7.rawValue:
+            tone = DTMF.tone7
+            break
+        case characterForTone.tone8.rawValue:
+            tone = DTMF.tone8
+            break
+        case characterForTone.tone9.rawValue:
+            tone = DTMF.tone9
+            break
+        case characterForTone.tone0.rawValue:
+            tone = DTMF.tone0
+            break
+        case characterForTone.toneA.rawValue:
+            tone = DTMF.toneA
+            break
+        case characterForTone.toneB.rawValue:
+            tone = DTMF.toneB
+            break
+        case characterForTone.toneC.rawValue:
+            tone = DTMF.toneC
+            break
+        case characterForTone.toneD.rawValue:
+            tone = DTMF.toneD
+            break
+        case characterForTone.toneStar.rawValue:
+            tone = DTMF.toneStar
+            break
+        case characterForTone.tonePound.rawValue:
+            tone = DTMF.tonePound
+            break
+        default:
+            break
+        }
+
+        return tone
+    }
+
+    public static func tonesForString(string: String) -> [DTMFType]?
+    {
+        var tones = [DTMFType]()
+        for character in string.characters {
+            if let tone = DTMF.toneForCharacter(character) {
+                tones.append(tone)
+            }
+        }
+
+        return tones.count > 0 ? tones : nil
+    }
+}
